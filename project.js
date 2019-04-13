4a2ed7a6cd15edeaa14f3045793ef7c456ace033
RJB <rboucher30@gmail.com>
2019-04-13 12:12:07 -0400

    fixed webhook error

--- project.js
+++ project.js
@@ -19,6 +19,6 @@ module.exports = {
         }
     },
     //arn:aws:lambda:us-east-1:670378695665:function:Lina
-    endpoint: '$(JOVO_WEBHOOK)',
+    endpoint: 'arn:aws:lambda:us-east-1:670378695665:function:Lina',
 };
  
